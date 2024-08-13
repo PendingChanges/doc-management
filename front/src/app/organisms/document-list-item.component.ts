e2a9f757ab65aa2081134b3f8e834e2c731d1c8b
Rémi Aubert <aubert.remi@gmail.com>
2024-08-13 14:49:40 +0200

    :lipstick: revamp document list item component

--- front/src/app/organisms/document-list-item.component.ts
+++ front/src/app/organisms/document-list-item.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
 
@@ -7,6 +7,8 @@ import { Document } from '../../graphql/generated';
 import { environment } from '../../infrastructure/environments/environment.development';
 import { DocumentIconComponent } from '../molecules/document-icon.component';
 import { RouterModule } from '@angular/router';
+import { MenuModule } from 'primeng/menu';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-document-list-item',
@@ -17,47 +19,51 @@ import { RouterModule } from '@angular/router';
     CommonModule,
     ToolbarModule,
     RouterModule,
+    MenuModule,
   ],
-  template: ` <a
-    [routerLink]="documentInfosUrl()"
-    rel="noopener noreferrer"
-    class="no-underline"
-    ><div
-      class="flex flex-column sm:flex-row sm:align-items-center hover:surface-100 cursor-pointer"
-      [ngClass]="{ 'border-top-1 surface-border': !first() }"
+  template: `<div
+    class="flex hover:surface-100 cursor-pointer"
+    [ngClass]="{ 'border-top-1 surface-border': !first() }"
+  >
+    <a
+      [routerLink]="documentInfosUrl()"
+      rel="noopener noreferrer"
+      class="flex-grow-1 no-underline"
     >
-      <div class="md:w-10rem relative">
-        <app-document-icon [document]="document()" />
-      </div>
-      <div
-        class="flex flex-column md:flex-row justify-content-between md:align-items-center flex-1"
-      >
-        <div
-          class="flex flex-row md:flex-column justify-content-between align-items-start gap-2 text-lg font-medium text-900"
-        >
+      <div class="flex ">
+        <div><app-document-icon [document]="document()" /></div>
+        <div class="text-lg font-medium text-900">
           {{ document()?.name }}
         </div>
-        <div class="flex flex-column md:align-items-end">
-          <div class="flex flex-row-reverse md:flex-row">
-            <a
-              [href]="documentUrl()"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="p-button mr-2"
-            >
-              <i class="pi pi-external-link"></i>
-            </a>
-          </div>
-        </div>
-      </div></div
-  ></a>`,
+      </div>
+    </a>
+    <div>
+      <a [href]="documentUrl()" target="_blank">
+        <span
+          class="p-button-icon pi pi-external-link"
+          aria-hidden="true"
+        ></span
+      ></a>
+    </div>
+  </div> `,
   styles: '',
 })
-export class DocumentListItemComponent {
+export class DocumentListItemComponent implements OnInit {
+  items: MenuItem[] | undefined;
+
   document = input<Document>();
   first = input<boolean>();
   documentUrl = computed(
     () => `${environment.apiUrl}/documents/${this.document()?.id}`
   );
   documentInfosUrl = computed(() => `/documents/${this.document()?.id}`);
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Open in new tab',
+        icon: 'pi pi-external-link',
+        url: this.documentUrl(),
+      },
+    ];
+  }
 }
